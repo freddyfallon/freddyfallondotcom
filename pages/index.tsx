637f41e6d5e9f369ab1bbd96ab1f677af999c7fc
Freddy Fallon <freddyfallon@gmail.com>
2024-02-16 13:02:29 +0000

    Redesign page to have some text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,41 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 function Header(props: { children: React.ReactNode }) {
-  return <div className="flex justify-center py-5">{props.children}</div>;
+  return <header className="flex justify-between flex-col gap-3 sm:flex-row sm:gap-0">{props.children}</header>;
 }
 
 function Nav(props: { children: React.ReactNode }) {
   return (
-    <div className="flex justify-center">
-      <nav className="flex justify-center w-4/5 sm:w-3/5 space-x-2 sm:space-x-3 md:space-x-6 lg:space:x-5 xl:space:x-6">
-        {props.children}
-      </nav>
-    </div>
+    <nav className="flex sm:justify-end items-center space-x-2 md:space-x-4 lg:space-x-8">
+      {props.children}
+    </nav>
   );
 }
 
 function NavLink(props: { href: string; children: React.ReactNode }) {
   return (
-    <a className="text-sm sm:text-base md:text-3xl" href={props.href}>
+    <a className="text-sm sm:text-base md:text-xl lg:text2xl xl:text3xl hover:text-slate-500" href={props.href}>
       {props.children}
     </a>
   );
 }
 
+function Section(props: { children: React.ReactNode }) {
+  return <section className="mt-8">{props.children}</section>;
+}
+
+function VSpace(props: { children: React.ReactNode }) {
+  return <div className="space-y-4">{props.children}</div>;
+}
+
+function Text(props: { children: React.ReactNode }) {
+  return <p className="text-base sm:text-lg md:text-xl lg:text-2xl">{props.children}</p>;
+}
+
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center h-full font-serif space-y-0 sm:space-y-3 md:space-4 lg:space-8">
+    <main className="font-serif text-slate-800 py-5 px-9">
       <Head>
         <title>Freddy Fallon - Software Engineer</title>
         <meta
@@ -45,17 +56,29 @@ export default function Home() {
         />
       </Head>
       <Header>
-        <h1 className="text-center text-4xl font-bold sm:text-5xl md:text-7xl lg:text-9xl">
+        <h1 className="text-4xl font-bold sm:text-4xl md:text-5xl lg:text-7xl">
           <a href="/">Freddy Fallon</a>
         </h1>
+        <Nav>
+          <NavLink href="https://www.github.com/freddyfallon/">GitHub</NavLink>
+          <NavLink href="https://www.linkedin.com/in/freddyfallon/">
+            LinkedIn
+          </NavLink>
+          <NavLink href="mailto:freddyfallon@gmail.com">Email</NavLink>
+        </Nav>
       </Header>
-      <Nav>
-        <NavLink href="https://www.github.com/freddyfallon/">GitHub</NavLink>
-        <NavLink href="https://www.linkedin.com/in/freddyfallon/">
-          LinkedIn
-        </NavLink>
-        <NavLink href="mailto:freddyfallon@gmail.com">Email</NavLink>
-      </Nav>
-    </div>
+      <Section>
+        <VSpace>
+          <Text>Hello 👋</Text>
+          <Text>I'm Freddy. A software engineer based in the U.K. I'm based in Brighton but normally work in
+            London.</Text>
+          <Text>I work with JavaScript and TypeScript, as well as Go. I've worked at a number of places, such as Bulb
+            🪦, Compare The Market 👔, and Monzo 💰.</Text>
+          <Text>If you want to get in touch, feel free to <Link className="underline decoration-pink-500 text-pink-500"
+                                                                href="mailto:freddyfallon@gmail.com">email me</Link>, or
+            check out my <Link className="underline decoration-pink-500 text-pink-500" href="https://www.linkedin.com/in/freddyfallon/">LinkedIn</Link>.</Text>
+        </VSpace>
+      </Section>
+    </main>
   );
 }
